d6745ac357c8cca9add5b7c7b7ee2d894f032672
solimartu <soli13@gmail.com>
2021-12-28 10:50:56 +0100

    lets see

--- fintechapp/pages/index.js
+++ fintechapp/pages/index.js
@@ -3,5 +3,22 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="container mx-auto bg-[#31ba9c]">
+      <h2 className="font-bold text-white text-2xl text-center mt-2 pt-6">
+        Hoy es el ultimo dia
+      </h2>
+      <h2 className="font-bold text-white text-4xl text-center -mt-5 pt-6">
+        de tu vida de pobre
+      </h2>
+      <Image
+        priority
+        src="/images/financialimg2.webp"
+        className="img-fluid mx-auto text-center"
+        height={783}
+        width={1024}
+        alt="bla"
+      />
+    </div>
+  );
 }
--- fintechapp/pages/profile.js
+++ fintechapp/pages/profile.js
@@ -18,17 +18,17 @@ import { AssessmentAnswer } from "@prisma/client";
 //       props: data,
 //     };
 //   }
-export async function getStaticProps() {
-  try {
-    const assessments = await prisma.assessmentAnswer.findMany({});
+// export async function getStaticProps() {
+//   try {
+//     const assessments = await prisma.assessmentAnswer.findMany({});
 
-    return {
-      props: { assessments },
-    };
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     return {
+//       props: { assessments },
+//     };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 // function Assessment() {
 //   // `data` will always be available as it's in `fallback`.
@@ -36,28 +36,59 @@ export async function getStaticProps() {
 //   return <h1>{data.ingresos}</h1>;
 // }
 
-export default function Profile(props) {
-  // SWR hooks inside the `SWRConfig` boundary will use those values.
+// export default function Profile(props) {
+//   // SWR hooks inside the `SWRConfig` boundary will use those values.
+//   return (
+//     <div>
+//       <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
+//         {props.assessments}
+//       </div>
+//     </div>
+//   );
+// }
+
+export default function Profile() {
   return (
-    <div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
-        {props.assessments}
+    <div className="container mx-auto">
+      <h2 className="font-bold text-black text-2xl text-center mt-2">
+        Tu perfil es:
+      </h2>
+      <div className="grid gap-4 grid-cols-3 mt-3">
+        <div className="flex items-center justify-center rounded-xl text-white text-2xl bg-emerald-300 flex-col">
+          <h5 className="font-bold text-xl">Tus ingresos:</h5>
+          <h3 className="font-extrabold text-5xl">Fijos</h3>
+        </div>
+        <div className="flex items-center justify-center rounded-xl text-white text-4xl font-extrabold bg-emerald-400  flex-col">
+          <div className="flex flex-col text-center">
+            <h3 className="text-5xl font-extrabold">60%</h3>
+            <h6 className="font-normal text-xs">destinado a gastos </h6>
+            <h5 className="text-xl font-extrabold uppercase">fijos</h5>
+          </div>
+          <div className="flex flex-col text-center">
+            <h3 className="text-5xl font-extrabold">20%</h3>
+            <h6 className="font-normal text-xs">destinado a gastos </h6>
+            <h5 className="text-xl font-extrabold uppercase">
+              variables imprescindibles
+            </h5>
+          </div>
+          <div className="flex flex-col text-center">
+            <h3 className="text-5xl font-extrabold">20%</h3>
+            <h6 className="font-normal text-xs">destinado a gastos </h6>
+            <h5 className="text-xl font-extrabold uppercase">
+              variables prescindibles
+            </h5>
+          </div>
+        </div>
+        <div className="flex items-center justify-center rounded-xl text-white bg-emerald-500 flex-col text-center">
+          <h5 className="font-bold text-xl">Tus objetivos:</h5>
+          <h3 className="text-5xl font-extrabold">1</h3>
+          <h5 className="text-2xl font-extrabold">Ahorrar</h5>
+
+          <h3 className="text-5xl font-extrabold">2</h3>
+          <h5 className="text-2xl font-extrabold">Administrar mis finanzas</h5>
+        </div>
       </div>
+      <button className="text-black text-center">Y ahora qué?</button>
     </div>
   );
 }
-
-//   return (
-//     <div>
-//       <h2>Tu perfil</h2>
-//       <p>Ingresos:</p>
-//       <p>{props}</p>
-//       <br />
-//       <p>Tus gastos:</p>
-//       <p></p>
-//       <br />
-//       <p>Tus objetivos:</p>
-//       <p></p>
-//       <button className="bg-red">Y ahora qué?</button>
-//     </div>
-//   );
