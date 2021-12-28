59e12a7e6960d8486f6d2bfe6a60a664f34dd9fc
Corinna-Genchi <corinnagenchi@gmail.com>
2021-12-28 12:23:05 +0100

    questions working with prisma!

--- fintechapp/pages/questions.js
+++ fintechapp/pages/questions.js
@@ -1,24 +1,35 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import useSWR from "swr";
+import { PrismaClient } from "@prisma/client";
 
 export async function getServerSideProps(context) {
-  const apiUrl = "http://localhost:3000/api/questions";
-  // await axios.get(apiUrl).then((response) => setQuestions(response.data));
-  const response = await fetch(apiUrl)
-  const questions = await response.json()
-  console.log(questions);
-  return { props: { questions } };
+  // const apiUrl = "http://localhost:3000/api/questions";
+  // // await axios.get(apiUrl).then((response) => setQuestions(response.data));
+  // const response = await fetch(apiUrl)
+  // const questions = await response.json()
+  // console.log(questions);
+  // return { props: { questions } };
+
+    const prisma = new PrismaClient();
+    const questions = await prisma.question.findMany();
+    
+    return {
+      props: { questions },
+    };
+  
 }
 
 export default function Questions({questions}) {
+
+
  
 const showQuestions = () =>{
   return (
     <section>
       <ul>
         {questions.map((question) => (
-          <li key={question}>
+          <li key={question.id}>
             <h3>{question.title}</h3>
           </li>
         ))}
@@ -38,6 +49,7 @@ return (
       A continuacion te haremos unas preguntas para evaluar tu situacion
       financiera
     </section>
+    {/* <h2>{questions[1].title}</h2> */}
     <section>
        <ul>
          {questions.map((question) => (           
@@ -48,8 +60,8 @@ return (
        </ul>
      </section>
 
-  
-    <button className="p-3 bg-gray-200" onClick={showQuestions}>Start assesment</button>
+{/*   
+    <button className="p-3 bg-gray-200" onClick={showQuestions}>Start assesment</button> */}
   </div>
 );
 }
