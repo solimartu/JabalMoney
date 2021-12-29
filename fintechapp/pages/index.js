4b466ee6d8b39cd284762a42a2b710a78aacfee9
solimartu <soli13@gmail.com>
2021-12-29 17:44:41 +0100

    favico

--- fintechapp/pages/index.js
+++ fintechapp/pages/index.js
@@ -3,11 +3,10 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
   return (
     <div className="container mx-auto bg-[#31ba9c]">
       <h2 className="font-bold text-white text-2xl text-center mt-2 pt-6">
-        Hoy es el ultimo dia
+        Hoy es el último día
       </h2>
       <h2 className="font-bold text-white text-4xl text-center -mt-5 pt-6">
         de tu vida de pobre
@@ -23,31 +22,15 @@ export default function Home() {
     </div>
   );
 
-
-
-
-
-// export default async function getServerSideProps() {
-//   // Fetch data from external API
-//   const apiUrl = "http://localhost:3000/api/questions";
-//   const response = await fetch(apiUrl);
-//   const data = await response.json();
-//   console.log(data);
-//   //  setQuestions(data);
-//   //  console.log(questions);
-// }
-
-
-
-
-
-
-
-
-
-
+  // export default async function getServerSideProps() {
+  //   // Fetch data from external API
+  //   const apiUrl = "http://localhost:3000/api/questions";
+  //   const response = await fetch(apiUrl);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   //  setQuestions(data);
+  //   //  console.log(questions);
+  // }
 
   return <div></div>;
-
-
 }
