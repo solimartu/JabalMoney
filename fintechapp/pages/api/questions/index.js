2a4461a174e78c3dcc0bd996681a48ac72c1dfcf
solimartu <soli13@gmail.com>
2022-01-04 15:39:23 +0100

    after realizing the db should be not in the fe

--- fintechapp/pages/api/questions/index.js
+++ fintechapp/pages/api/questions/index.js
@@ -1,9 +1,40 @@
 const { PrismaClient } = require("@prisma/client");
-
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-  const questions = await prisma.question.findMany();
-  res.json(questions);
-}
+  const { incomes } = req.body;
 
+  // try {
+  const answers = await prisma.assessmentAnswer.create({
+    data: {
+      user: {
+        connect: {
+          id: 2,
+        },
+      },
+      incomes,
+      percentfixedoutcomes: 0.5,
+      percentessentialoutcomes: 0.3,
+      percentexpendableoutcomes: 0.2,
+      objective1: "AdministrarMisFinanzas",
+      objective2: "SalirDeDeudas",
+      objective3: "Ahorrar",
+    },
+  });
+  console.log("el reqbody", req.body);
+  console.log("lasansers", answers);
+}
+//   res.status(200).json(answers);
+// } catch (error) {
+//   console.log(error);
+//   res.status(400).json({ err });
+// }
+// }
+handle()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
