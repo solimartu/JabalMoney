5f3d41782fb5a86e9c3db0f47e26db051682a3dc
Corinna-Genchi <corinnagenchi@gmail.com>
2022-01-07 17:06:35 +0100

    trying changing everything to serverside

---
+++ fintechapp/pages/api/questions/[id].js
@@ -0,0 +1,37 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+export default async function handle(req, res) {
+  const { id } = req.params;
+
+  try {
+    const question = await prisma.question.findMany({
+      where: {
+        id: +id,
+      },
+    });
+    const opcion = await prisma.opcion.findMany({
+      where: {
+        questionId: id,
+      },
+    });
+
+    const questionnaire = {
+      question: question,
+      opcion: opcion,
+    };
+    res.status(200).json(questionnaire);
+    console.log("a ver que polla es esto questionnaire", questionnaire);
+  } catch (error) {
+    console.log("este seria el error", error);
+    res.status(400).json({ error });
+  }
+  handle()
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+}
--- fintechapp/pages/api/questions/index.js
+++ fintechapp/pages/api/questions/index.js
@@ -2,48 +2,71 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
+  const { query } = req;
+  const { id } = query;
 
-//call to prisma 
- const prisma = new PrismaClient();
- const question = await prisma.question.findFirst();
- const questions = [question];
+  //go to question table
+  const prisma = new PrismaClient();
 
+  try {
+    const question = await prisma.question.findFirst();
+    const questions = [question];
+    // console.log("esto es question", question);
+    // //opcion table
+    // const opcion = await prisma.opcion.findMany({
+    //   // where: {
+    //   //   questionId: id,
+    //   // },
+    // });
+    res.status(200).json(questions);
+    console.log("esto es opcion", opcion);
+  } catch (error) {
+    console.log("este seria el error", error);
+    res.status(400).json({ error });
+  }
+  handle()
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
 
+  //   const { incomes } = req.body;
 
-//   const { incomes } = req.body;
-
-//   // try {
-//   const answers = await prisma.assessmentAnswer.create({
-//     data: {
-//       user: {
-//         connect: {
-//           id: 2,
-//         },
-//       },
-//       incomes,
-//       percentfixedoutcomes: 0.5,
-//       percentessentialoutcomes: 0.3,
-//       percentexpendableoutcomes: 0.2,
-//       objective1: "AdministrarMisFinanzas",
-//       objective2: "SalirDeDeudas",
-//       objective3: "Ahorrar",
-//     },
-//   });
-//   console.log("el reqbody", req.body);
-//   console.log("lasansers", answers);
-// }
-// //   res.status(200).json(answers);
-// // } catch (error) {
-// //   console.log(error);
-// //   res.status(400).json({ err });
-// // }
-// // }
-// handle()
-//   .catch((e) => {
-//     console.error(e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+  //   // try {
+  //   const answers = await prisma.assessmentAnswer.create({
+  //     data: {
+  //       user: {
+  //         connect: {
+  //           id: 2,
+  //         },
+  //       },
+  //       incomes,
+  //       percentfixedoutcomes: 0.5,
+  //       percentessentialoutcomes: 0.3,
+  //       percentexpendableoutcomes: 0.2,
+  //       objective1: "AdministrarMisFinanzas",
+  //       objective2: "SalirDeDeudas",
+  //       objective3: "Ahorrar",
+  //     },
+  //   });
+  //   console.log("el reqbody", req.body);
+  //   console.log("lasansers", answers);
+  // }
+  // //   res.status(200).json(answers);
+  // // } catch (error) {
+  // //   console.log(error);
+  // //   res.status(400).json({ err });
+  // // }
+  // // }
+  // handle()
+  //   .catch((e) => {
+  //     console.error(e);
+  //     process.exit(1);
+  //   })
+  //   .finally(async () => {
+  //     await prisma.$disconnect();
+  //   });
 }
