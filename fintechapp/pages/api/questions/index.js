4108532adafb07f9dfd7f959a900b6fbeff22e15
Corinna-Genchi <corinnagenchi@gmail.com>
2022-01-07 13:53:23 +0100

    seeding and migrate

--- fintechapp/pages/api/questions/index.js
+++ fintechapp/pages/api/questions/index.js
@@ -2,39 +2,48 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-  const { incomes } = req.body;
 
-  // try {
-  const answers = await prisma.assessmentAnswer.create({
-    data: {
-      user: {
-        connect: {
-          id: 2,
-        },
-      },
-      incomes,
-      percentfixedoutcomes: 0.5,
-      percentessentialoutcomes: 0.3,
-      percentexpendableoutcomes: 0.2,
-      objective1: "AdministrarMisFinanzas",
-      objective2: "SalirDeDeudas",
-      objective3: "Ahorrar",
-    },
-  });
-  console.log("el reqbody", req.body);
-  console.log("lasansers", answers);
+//call to prisma 
+ const prisma = new PrismaClient();
+ const question = await prisma.question.findFirst();
+ const questions = [question];
+
+
+
+//   const { incomes } = req.body;
+
+//   // try {
+//   const answers = await prisma.assessmentAnswer.create({
+//     data: {
+//       user: {
+//         connect: {
+//           id: 2,
+//         },
+//       },
+//       incomes,
+//       percentfixedoutcomes: 0.5,
+//       percentessentialoutcomes: 0.3,
+//       percentexpendableoutcomes: 0.2,
+//       objective1: "AdministrarMisFinanzas",
+//       objective2: "SalirDeDeudas",
+//       objective3: "Ahorrar",
+//     },
+//   });
+//   console.log("el reqbody", req.body);
+//   console.log("lasansers", answers);
+// }
+// //   res.status(200).json(answers);
+// // } catch (error) {
+// //   console.log(error);
+// //   res.status(400).json({ err });
+// // }
+// // }
+// handle()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
 }
-//   res.status(200).json(answers);
-// } catch (error) {
-//   console.log(error);
-//   res.status(400).json({ err });
-// }
-// }
-handle()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
