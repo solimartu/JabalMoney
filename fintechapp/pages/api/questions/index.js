67c9bf72384f5fa16a21a95a4e363f5318076eb4
solimartu <soli13@gmail.com>
2022-01-14 10:53:22 +0100

    un error no es cuestion de decir ve todo a la mierda.CG

--- fintechapp/pages/api/questions/index.js
+++ fintechapp/pages/api/questions/index.js
@@ -1,23 +1,34 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default async function handle(req, res) {
-  try {
-    const question = await prisma.question.findFirst();
-    const questions = [question];
-
-    return res.status(200).json(questions);
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-  handle()
-    .catch((e) => {
-      console.error(e);
-      process.exit(1);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
+  const session = await getSession({ req });
+  if (session) {
+    res.send({
+      content: "Welcome to the secret page",
     });
+    try {
+      const question = await prisma.question.findFirst();
+      const questions = [question];
+
+      return res.status(200).json(questions);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+    handle()
+      .catch((e) => {
+        console.error(e);
+        process.exit(1);
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+  } else {
+    res.send({
+      error: "You need to be signed in.",
+    });
+  }
 
   //   const { incomes } = req.body;
 
